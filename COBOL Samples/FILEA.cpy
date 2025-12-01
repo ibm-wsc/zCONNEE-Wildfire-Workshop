65b518c8693d49b33d1d74cc09c2da8a2d20011c
emitchj <emitchj@gmail.com>
2025-12-01 07:59:07 -0500

    Delete COBOL Samples directory

--- COBOL Samples/FILEA.cpy
+++
@@ -1,9 +0,0 @@
-       01  FileA-record.
-               05  stat         PIC X.
-               05  numb         PIC X(6).
-               05  name         PIC X(20).
-               05  addrx        PIC X(20).
-               05  phone        PIC X(8).
-               05  datex        PIC X(8).
-               05  amount       PIC X(8).
-               05  comment      PIC X(9).
--- COBOL Samples/PART.cbl
+++
@@ -1,6 +0,0 @@
-       01 PART-REC.                                                     00010000
-          03 PART-KEY PIC X(17).                                        00020000
-          03 PART-KEY-DETAIL REDEFINES PART-KEY.                        00030000
-             05 PART-PREFIX            PIC X(02).                       00040000
-             05 PART-NUMBER            PIC X(15).                       00050000
-          03 FILLER.                                                    00060000
